fd4f36f7ae86d00d294908f1957fad8eed95229f
cinesubzcloudsystem1 <92335282+cinesubzcloudsystem1@users.noreply.github.com>
2021-10-14 09:51:43 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🎖 DEPLOY YOURS 🎖', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('🎖 My Channel 🎖', url=f'https://t.me/cinesubz')
                     ]
                     ]
                 await bot.send_cached_media(
